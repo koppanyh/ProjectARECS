efda484e9426d8deb2d90f1d6744f63baadb929b
Koppany Horvath <koppanyh12@gmail.com>
2018-03-11 19:58:59 -0700

    Day 2 of development
    Finished the login system and started working on cosmetic stuff.

--- arecs/index.js
+++ arecs/index.js
@@ -1,15 +1,42 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const session = require('client-sessions');
+const fs = require('fs');
+const mysql = require('mysql');
+const crypto = require('crypto');
 
 var app = express();
 app.use(express.static("public"));
 var server;
 var urlencodedParser = bodyParser.urlencoded({extended: false});
 
+///////////////////////////////// mysql stuff ////////////////////////////////////
+var mysqlhost = "31.220.105.80";
+var mysqluser = "khlabsor_arecsUser";
+var mysqldatb = "khlabsor_arecs";
+var conn = null;
+fs.readFile("db.key", function(err, dat){
+	conn = mysql.createConnection({
+		host: mysqlhost,
+		database: mysqldatb,
+		user: mysqluser,
+		password: dat.toString()
+	});
+	conn.connect(function(err){
+		if(err) console.error(err);
+		else console.log("Connected to MySQL");
+	});
+});
+
 process.on("SIGINT", function(){
 	//add all cleanup code here
-	process.exit();
+	console.log("");
+	conn.end(function(err){
+		if(err) console.error(err);
+		console.log("MySQL connection closed");
+		process.exit();
+	});
+	setTimeout(function(){ process.exit(); }, 3000);
 });
 
 app.use(session({
@@ -21,34 +48,56 @@ app.use(session({
 }));
 
 app.get('/', function(req, res){
-	console.log(req.session);
-	res.send(Math.random() + ' Have you been <a href="/account">logged in</a> today?<br>\n');
+	if(isLoggedIn(req)) res.redirect("/clockin.html");
+	else res.redirect("/account");
 });
 
-//account, post only
-function loggedInRedir(req, res){
-	if(req.session && req.session.user) res.redirect("/account/user.html"); //if logged in then redir to user
-	else res.redirect("/account/login.html"); //else redir to login
-}
+///////////////////////////////// account stuff /////////////////////////////
+function isLoggedIn(req){ return req.session && req.session.user; }
 app.post('/account', urlencodedParser, function(req, res){
-	console.log(req.body);
 	if(req.body.action == "login"){
-		req.session.user = {email: req.body.email};
-		res.redirect('/account/user.html');
+		conn.query("SELECT * from userdb WHERE email=?", [req.body.email], function(err, result){
+			if(result.length == 0) res.redirect("/account/login.html?err1");
+			else{
+				result = result[0];
+				var hash = crypto.createHmac('sha512', result.salt);
+				hash.update(req.body.passw);
+				if(hash.digest("hex") == result.hashpw){
+					delete result.salt;
+					delete result.hashpw;
+					delete result.uid;
+					result.days = JSON.parse(result.days);
+					req.session.user = result;
+					res.redirect('/clockin.html');
+				} else res.redirect("/account/login.html?err2");
+			}
+		});
+	} else if(req.body.action == "getuser"){
+		res.send(JSON.stringify(req.session.user));
 	} else if(req.body.action == "logout"){
-		req.session.reset();
+		req.redirect("account/logout");
 	} else{
 		res.redirect("/account/login.html");
 	}
 });
-app.get('/account', function(req, res){ loggedInRedir(req, res); });
+app.get('/account', function(req, res){
+	if(isLoggedIn(req)) res.redirect("/account/user.html"); //if logged in then redir to user
+	else res.redirect("/account/login.html"); //else redir to login
+});
 app.get('/account/logout', function(req, res){
 	req.session.reset();
-	res.redirect("/");
+	res.redirect("/account/login.html");
 });
-//make login redirect to user if logged in
-//make user redirects to login if not logged in
+app.get('/account/login.html', function(req, res){
+	if(isLoggedIn(req)) res.redirect("/account/user.html");
+	else res.sendFile(__dirname+"/account/login.html");
+});
+app.get('/account/user.html', function(req, res){
+	if(isLoggedIn(req)) res.sendFile(__dirname+"/account/user.html");
+	else res.redirect("/account/login.html");
+});
 
+///////////////////////////////////// api stuff //////////////////////////////////
 function apiFunc(req, res){
 	var body = "";
 	
